55d82204c7c117044201827c91c40b42736eee5e
Jorge Hernández Ríos <jorge.hernandezr@sngular.com>
2023-08-16 18:19:31 +0100

    [NO-ISSUE] Improvements on controller arguments listener

---
+++ src/Pagination/GetPaginatedResponseDataTrait.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ecosystem\ApiHelpersBundle\Pagination;
+
+trait GetPaginatedResponseDataTrait
+{
+    private function getPaginatedResponseData(array $items, int $total, ?int $limit, ?int $page): array
+    {
+        return [
+            'items' => $items,
+            'total' => $total,
+            'limit' => $limit,
+            'page' => $page ?? 1,
+            'pages' => $limit !== null ? ceil($total / $limit) : 1,
+        ];
+    }
+}
