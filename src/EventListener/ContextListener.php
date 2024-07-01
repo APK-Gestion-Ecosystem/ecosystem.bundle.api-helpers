22abfa817fdfc8cca2489ca1e752d2c224f98d34
Daniel Córdoba <daniel.cordoba@sngular.com>
2024-07-01 13:30:08 +0200

    [AE-963] catalan locale

--- src/EventListener/ContextListener.php
+++ src/EventListener/ContextListener.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 class ContextListener
 {
     private const LOCALE_HEADER = 'X-ECOSYS-LOCALE';
-    private const ALLOWED_LOCALES = ['es', 'en'];
+    private const ALLOWED_LOCALES = ['es', 'en', 'ca'];
 
     public function __construct(private readonly ContextService $contextService)
     {
